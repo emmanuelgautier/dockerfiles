fee1471a430859f43f19f9c1c0f444c47aa7617b
Emmanuel Gautier <git@emmanuelgautier.fr>
2017-11-26 13:28:33 +0100

    allow update backup and fix some bugs

--- hubic-backup/docker-entrypoint.sh
+++ hubic-backup/docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 PASSWORD_PATH="/root/.hubic_pwd"
+HUBIC_BACKUP_PATH="/hubic"
 
 /etc/init.d/dbus start
 
@@ -15,12 +16,33 @@ function hubic_init() {
 
   dbus_init
   hubic login --password_path=$PASSWORD_PATH $HUBIC_EMAIL
-  hubic stop
 }
 
 sleep 2
 
 hubic_init
 
+if hubic backup info | awk 'FNR > 1 {print $1}' | grep -Fxq $HUBIC_BACKUP_NAME
+then
+  echo "Updating backup $HUBIC_BACKUP_NAME config"
+
+  hubic backup attach $HUBIC_BACKUP_NAME $HUBIC_BACKUP_PATH
+  hubic backup config --drop_deleted \
+    --path=$HUBIC_BACKUP_PATH \
+    --frequency=$HUBIC_BACKUP_FREQUENCY \
+    --kept_versions=$HUBIC_BACKUP_KEPT_VERSIONS \
+    $HUBIC_BACKUP_PATH
+
+  hubic backup update $HUBIC_BACKUP_PATH
+else
+  echo "Creating backup $HUBIC_BACKUP_NAME"
+
+  hubic backup create --drop_deleted \
+    --name=$HUBIC_BACKUP_NAME \
+    --frequency=$HUBIC_BACKUP_FREQUENCY \
+    --kept_versions=$HUBIC_BACKUP_KEPT_VERSIONS \
+    $HUBIC_BACKUP_PATH
+fi
+
 dbus_init
-hubic backup create --drop_deleted --name=$HUBIC_BACKUP_NAME --frequency=$HUBIC_BACKUP_FREQUENCY --kept_versions=$HUBIC_BACKUP_KEPT_VERSIONS -- /hubic
+hubic main-loop --verbose
